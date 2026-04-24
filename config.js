a3806987f27a33db32c7b53addba3fc2c3a7c9b0
EmptyNeo <dmitryaen1@mail.ru>
2026-04-24 18:17:53 +0300

    fix

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 module.exports = {
   supabaseUrl: "https://cwdrpuojiwsjxgdpdxqo.supabase.co",
-  proxyEndpoint: "/proxy",
-  anonApiKey: "sb_publishable_HZb6vCppc7zxYSZDaagv3w_hhsZg80Q",
+  cookieConfig: {
+    secret: "some_cookie_secret",  // можно оставить любое значение
+    path: "/"
+  },
+  proxyConfig: {
+    anonApiKey: "sb_publishable_HZb6vCppc7zxYSZDaagv3w_hhsZg80Q",
+  },
   gatewayConfig: {
     host: "0.0.0.0",
     port: process.env.PORT || 8080
+  },
+  serverConfig: {
+    logger: true
   }
 };
