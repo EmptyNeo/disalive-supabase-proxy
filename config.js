57aca87a917e228338105f66d14b38e72824e797
EmptyNeo <dmitryaen1@mail.ru>
2026-04-24 17:58:42 +0300

    fix

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   supabaseUrl: "https://cwdrpuojiwsjxgdpdxqo.supabase.co",
-  proxyEndpoint: "/proxy"
+  proxyEndpoint: "/proxy",
+  gatewayConfig: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 8080
+  }
 };
